35c886c5390d6354773572549056a37892ced06d
zack <zengyangzhao@outlook.com>
2017-08-02 08:18:58 +0800

    cucumber add datatable

--- features/step_definitions/post.js
+++ features/step_definitions/post.js
@@ -2,50 +2,79 @@ var driver = require('../support/world').driver;
 var By = require('../support/world').By;
 var assert = require('assert');
 
-module.exports = function() {
+module.exports = function () {
 
 
-    this.Given(/^点击发布话题按钮$/, function () {
-      return driver.findElement(By.css(".span-success")).click();
-    });
+  this.Given(/^点击发布话题按钮$/, function () {
+    return driver.findElement(By.css(".span-success")).click();
+  });
 
-    this.When(/^板块选择分享$/, function () {
-      driver.findElement(By.id('tab-value')).click();
-      return driver.findElement(By.css("#tab-value > option:nth-child(2)")).click();
-    });
+  this.When(/^板块选择分享$/, function () {
+    driver.findElement(By.id('tab-value')).click();
+    return driver.findElement(By.css("#tab-value > option:nth-child(2)")).click();
+  });
 
-    this.Then(/^标题输入"([^"]*)"$/, function (arg1) {
-      // Write code here that turns the phrase above into concrete actions
-      return driver.findElement(By.id("title")).sendKeys(arg1);
-    });
+  this.Then(/^标题输入"([^"]*)"$/, function (arg1) {
+    // Write code here that turns the phrase above into concrete actions
+    return driver.findElement(By.id("title")).sendKeys(arg1);
+  });
 
-    this.Then(/^内容输入"([^"]*)"$/, function (arg1) {
-      // Write code here that turns the phrase above into concrete actions
-      return 'pending';
-    });
+  this.Then(/^内容输入"([^"]*)"$/, function (arg1) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
 
-    this.Given(/^打开帖子"([^"]*)"$/, function (arg1) {
-      // Write code here that turns the phrase above into concrete actions
-      return 'pending';
-    });
+  this.Given(/^打开帖子"([^"]*)"$/, function (arg1) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
 
-    this.Then(/^点击提交按钮$/, function () {
-      // Write code here that turns the phrase above into concrete actions
-      return driver.findElement(By.css("#create_topic_form > fieldset > div > div > div.editor_buttons > input")).click();
-    });
+  this.Then(/^点击提交按钮$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    return driver.findElement(By.css("#create_topic_form > fieldset > div > div > div.editor_buttons > input")).click();
+  });
 
-    this.When(/^添加回复内容"([^"]*)"$/, function (arg1) {
-      // Write code here that turns the phrase above into concrete actions
-      return 'pending';
-    });
+  this.When(/^添加回复内容"([^"]*)"$/, function (arg1) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
 
-    this.Then(/^点击回复按钮$/, function () {
-      // Write code here that turns the phrase above into concrete actions
-      return 'pending';
-    });
+  this.Then(/^点击回复按钮$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
 
-    this.Given(/^删除$/, function () {
-      // Write code here that turns the phrase above into concrete actions
-      return 'pending';
-    });
-};
+  this.Given(/^删除$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  this.Then(/^标题输入<datatable>$/, function (table) {
+    // Write code here that turns the phrase above into concrete actions
+    // for(var value of table.value ){
+    //   return driver.findElement(By.id("title")).sendKeys(value);
+    // }
+    console.log(table, table.rawTable)
+    for (var value of table.rawTable) {
+      for (var a of value) {
+        driver.findElement(By.id("title")).sendKeys(value.toString());
+      }
+    }
+
+  });
+
+
+  this.Then(/^分别输入用户名和密码$/, function (table) {
+    // console.log(table)
+    console.log("rows:",table.rows())
+    console.log("rowsHash",table.rowsHash())
+    console.log("raw",table.raw())
+    console.log('hashes',table.hashes())
+
+    let logininfo = table.rowsHash();
+    // loginingfo = { username: 'imzack', pass: '123456' }
+    driver.findElement(By.id("name")).sendKeys(logininfo.username)
+    return driver.findElement(By.id("pass")).sendKeys(logininfo.pass);
+  });
+
+}
---
+++ features/usedatatable.feature
@@ -0,0 +1,13 @@
+Feature: 用户登录 使用datatable
+参考：https://github.com/cucumber/cucumber-js/tree/v1.3.3
+
+https://github.com/cucumber/cucumber-js/blob/v1.3.3/docs/support_files/data_table_interface.md
+
+
+  Scenario: 登录
+    Given 打开网站首页
+    When 点击登录按钮
+    Then 分别输入用户名和密码
+    |username| imzack|
+    |pass|123456|
+    And 点击登录
